95a164fd65ef36f59df709c58617a704706a8d33
MatoskDev <guilhermematosgto2015@outlook.com>
2022-01-08 15:51:11 -0300

    commit v3

--- Jjs.js
+++ Jjs.js
@@ -1,8 +1,8 @@
 function ImgSlider (anything){
-    document.querySelector('.Starbucks').scr = anything;
+    document.querySelector('.Starbucks').src = anything;
 }
 
 function changeCcolor (color){
     const circle = document.querySelector ('.circle');
-    circle.style.backgroud = color;
+    circle.style.background = color;
 }
